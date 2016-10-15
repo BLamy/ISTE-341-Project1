a2a626351e2e6631524c272a6bceae27612d9870
Brett Lamy <blamy@calvauto.com>
2016-10-15 03:29:39 -0400

    Last bit of tweaking to template engine.

--- lib/TemplateEngine.php
+++ lib/TemplateEngine.php
@@ -17,14 +17,6 @@ class TemplateEngine {
   }
 
   /**
-   * Will take a component name and it's attributes then return the shallow rendered result.
-   */
-  public static function shallowRender($name, $attributes) {
-    include_once(self::pathForComponent($name));
-    return call_user_func($name, $attributes);
-  }
-
-  /**
    * Recursively find all the child nodes which are in the components directory
    */
   private static function findCustomChildrenOfNode($domNode) {
@@ -49,22 +41,26 @@ class TemplateEngine {
   * note: Mutates param
   */
   private static function compile($root) {
-    $elementName = $root->documentElement->localName;
     $children = self::findCustomChildrenOfNode($root->documentElement);
     foreach ($children as $child) {
       $attributes = [];
       foreach ($child->attributes as $attribute) {
         $attributes[$attribute->localName] = $attribute->value;
       }
-      if (self::isCustomElement($child->localName)) {
-        $jsx = self::shallowRender($child->localName, $attributes);
-        $tmp = new DomDocument();
-        $tmp->loadXML(self::render($jsx));
-        $node = $root->importNode($tmp->documentElement, true);
-        $child->parentNode->replaceChild($node, $child);
-      }
+      $tmp = new DomDocument();
+      $tmp->loadXML(self::mount($child->localName, $attributes));
+      $node = $root->importNode($tmp->documentElement, true);
+      $child->parentNode->replaceChild($node, $child);
     }
     return $root;
+  }
+
+  /**
+   * Will take a component name and it's attributes then return the shallow rendered result.
+   */
+  public static function shallowRender($name, $attributes) {
+    include_once(self::pathForComponent($name));
+    return call_user_func($name, $attributes);
   }
 
   /**
@@ -86,11 +82,19 @@ class TemplateEngine {
   }
 
   /**
+   * Will take a component name and it's attributes and return the deep rendered result.
+   * compose(self::render, self::shallowRender)
+   */
+  public static function mount($name, $attributes) {
+    return self::render(self::shallowRender($name, $attributes));
+  }
+
+  /**
    * Will Render given jsx into a valid html5 page
    */
-  public static function page($jsx, $pageName = '', $title='RIT Shirts') {
+  public static function page($jsx, $pageSlug = '', $title='RIT Shirts') {
     $body = self::render(<<<TEMPLATE
-      <div class="$pageName">
+      <div id="$pageSlug">
         <Toolbar />
         $jsx
       </div>
@@ -109,7 +113,7 @@ TEMPLATE
         </head>
         <body>
           $body
-          <script src="js/scripts.js"></script>
+          <script src="public/scripts.js"></script>
         </body>
       </html>
 PAGE;
